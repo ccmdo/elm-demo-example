59a4b4d9fa3b6303a2b85a920f83f743a3c4ccb8
Steven Hamilton <steven.hamilton@carwow.co.uk>
2018-09-04 11:18:59 +0100

    Empty subscriptions, and format

--- Compiles/Main.elm
+++ Compiles/Main.elm
@@ -2,29 +2,40 @@ module Main exposing (main)
 
 import Html exposing (Html, div, text)
 
+
 type alias Model =
-  { hello : String }
+    { hello : String }
+
 
 type Msg
-  = NoOp
+    = NoOp
 
-init : (Model, Cmd Msg)
+
+init : ( Model, Cmd Msg )
 init =
-  ( Model "Hello World"
-  , Cmd.none
-  )
+    ( Model "Hello World"
+    , Cmd.none
+    )
 
-update : Msg -> Model -> (Model, Cmd Msg)
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    NoOp ->
-      (model, Cmd.none)
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
+
 
 view : Model -> Html Msg
 view model =
-  div
-    []
-    [ text model.hello]
+    div
+        []
+        [ text model.hello ]
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
 
 main : Program Never Model Msg
 main =
@@ -32,5 +43,5 @@ main =
         { init = init
         , view = view
         , update = update
-        , subscriptions = always Sub.none
+        , subscriptions = subscriptions
         }
